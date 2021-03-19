2b1a25acec15a9366a855ebfcae61738a508cc47
rGudavadze <roma.gudavadze@gmail.com>
2021-03-19 16:44:07 +0400

    add login

--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,8 @@
 from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
+from flask_login import login_user
 
 @app.route('/')
 @app.route('/home')
@@ -22,7 +23,7 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                                 email_address=form.email_address.data,
-                                password_hash=form.password1.data)
+                                password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -30,3 +31,16 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f"There was an error with creating a user: {err_msg}", category='danger')
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash("Username and Password are not match!", category='danger')
+    return render_template('login.html', form=form)
